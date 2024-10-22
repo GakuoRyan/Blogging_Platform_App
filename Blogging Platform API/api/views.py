ea137bd639ada5b80fc16d33a294e99fedd5eb75
GakuoRyan <rwgakuo@gmail.com>
2024-10-22 12:38:52 +0300

    Update views.py

--- Blogging Platform API/api/views.py
+++ Blogging Platform API/api/views.py
@@ -7,53 +7,6 @@ from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-################################## Function base view (decorator view) #######################################
-
-
-
-# Create your views here.
-# @api_view(['GET','POST'])
-# def post_list(request):
-#     try:
-#         if request.method == 'GET':
-#             blog= Post.objects.all()
-#             serializer= PostSerializer(blog, many=True)
-#             return Response(serializer.data)
-#         elif request.method =='POST':
-#             serializer= PostSerializer(data=request.data)
-#             if serializer.is_valid():
-#                 serializer.save()
-#                 return Response({'payload':serializer.data,'status':201,'message':'Blog is created'})
-#             return Response({'payload':serializer.errors,'status':400,'message':'Something went Wrong'})
-#
-#     except Exception as e:
-#         return Response({'status':404,'message':'Not Found'})
-#
-#
-#
-#
-#
-# @api_view(['GET','PATCH','DELETE'])
-# def post_details(request,pk):
-#     try:
-#         blog= Post.objects.get(id=pk)
-#     except ObjectDoesNotExist:
-#         return Response({'status':404,'message':'Not Found'})
-#
-#     if request.method=='GET':
-#         serializer=PostSerializer(blog)
-#         return Response({'payload':serializer.data, 'status':200})
-#     elif request.method =='PATCH':
-#         serializer=PostSerializer(blog,data=request.data,partial=True)
-#         if not serializer.is_valid():
-#             return Response({'payload': serializer.errors, 'status': 400, 'message': 'Something went Wrong'})
-#         serializer.save()
-#         return Response({'message':serializer.data, 'status':200})
-#
-#     elif request.method=='DELETE':
-#         blog.delete()
-#         return Response({'message':'Blog successfully Deleted', 'status':200})
-
 
 class ViewList(APIView):
     def get(self,request):
